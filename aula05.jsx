4211dfea6b3047d2b769e328c8a441fd964cbf19
Erick2ds2025 <erick.hansen@escola.pr.gov.br>
2025-06-25 10:30:16 -0300

    Add files via upload

---
+++ aula05.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula05 extends Component{
+    render(){
+        return(
+            <View style={ styles.container }>
+                <View style={ styles.container }>1</View>
+                <View style={ styles.container }>2</View>
+                <View style={ styles.container }>3</View>
+            </View>
+        );
+    }
+}
+
+export default Aula05;
